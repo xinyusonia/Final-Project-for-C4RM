2d363f85ea2160b2ee814555cb33bf7ab624af90
xinyusonia <110503405+xinyusonia@users.noreply.github.com>
2022-08-09 16:54:56 -0400

    Add files via upload

---
+++ Final Project.R
@@ -0,0 +1,69 @@
+Inflation <- read.csv("https://github.com/xinyusonia/Final-Project-for-C4RM/blob/main/InflationRate.csv")
+
+RedFlags <- function(df, target, category){
+  categories    = df[,category]
+  uniques       = unique(categories)
+  
+  bigenough     = function(u) return(mean(categories==u)>0.01)
+  columnValue   = uniques[sapply(uniques, bigenough)]
+  
+  maxFor    = function(val) return(max(df[categories==val,target]))
+  maxTarget = sapply(columnValue, maxFor)
+  
+  columnName    = rep(category, length(columnValue))
+  newdf         = data.frame(columnName = columnName,
+                             columnValue = columnValue,
+                             maxTarget = maxTarget)
+  return(newdf)
+}
+
+RedFlagOverview <- function(df, target){
+  newdf = data.frame(columnName = c(), columnValue = c(), maxTarget = c())
+  for(name in names(df)[names(df) != target])
+    newdf = rbind(newdf, RedFlags(df, target, name))
+  worst = order(newdf$maxTarget)[1:10]
+  sorted = newdf[worst,]
+  row.names(sorted) = 1:nrow(sorted)
+  sorted$maxTarget = round(sorted$maxTarget, 3)
+  return(sorted)
+}
+
+
+library('scales')
+
+PortfolioPercentageChange = function(df){
+  ncol = ncol(df)
+  nrow = nrow(df)
+  for (col in 2:ncol){
+    for (row in nrow:2) {
+      df[row,col] = (df[row,col] - df[row-1,col]) / df[row-1,col]
+    }
+  }
+  df = df[2:nrow, 1:ncol]
+  newcol = ncol(df)
+  newrow = nrow(df)
+  for (col in 2:newcol){
+    for (row in 1:nrow) {
+        df[row,col] = percent(as.numeric(df[row,col]))
+    }
+  }
+  return(df)
+}
+# All data in one plot
+mydata <- melt(Inflation,id="Date")
+colnames(mydata) <- c("Date","Inflation","value")
+ggplot(data = mydata,aes(x=Date,
+                         y=value,
+                         group = Inflation,
+                         color=Inflation,
+                         shape=Inflation))+geom_point()+geom_line()
+
+# Plot for Overall Inflation & Unemployment Rate
+Inflation2<-subset(Inflation, select=c("Date", "OverallInflation","UnemploymentRate"))
+mydata2 <- melt(Inflation2,id="Date")
+colnames(mydata2) <- c("Date","Inflation","value")
+ggplot(data = mydata2,aes(x=Date,
+                         y=value,
+                         group = Inflation,
+                         color=Inflation,
+                         shape=Inflation))+geom_point()+geom_line()
